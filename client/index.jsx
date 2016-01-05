a821299ef0b35599d1f3699e7a873af92797b98c
ustccjw <317713370@qq.com>
2016-01-05 18:43:07 +0800

    remove scroll-behavior

--- client/index.jsx
+++ client/index.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router'
 import { createHistory } from 'history'
-import useScroll from 'scroll-behavior/lib/useStandardScroll'
 import AsyncProps from 'async-props'
 import routes from '../route'
 
 global.React = React
 
-const history = useScroll(createHistory)()
+const history = createHistory()
 
 const router = (
 	<Router routes={routes} history={history} RoutingContext={AsyncProps} />
