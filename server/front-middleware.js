aa5c7b8e5b0e81299006c28eaff5b54f942d550e
ustccjw <317713370@qq.com>
2016-01-06 21:55:11 +0800

    avoid multiple frontend watch when backend hot reload

--- server/front-middleware.js
+++ server/front-middleware.js
@@ -1,18 +1,26 @@
+// this file modify will trigger multiple frontend webpack watch
+// so it should restart server when this file is modified
+
 import webpack from 'webpack'
+import webpackDevMiddleware from 'webpack-dev-middleware'
+import webpackHotMiddleware from 'webpack-hot-middleware'
 import config from '../webpack.config.frontend'
 
+const compiler = webpack(config)
+const devMiddleware = webpackDevMiddleware(compiler, {
+	publicPath: config.output.publicPath,
+	stats: {
+		cached: false,
+		cachedAssets: false,
+		colors: true,
+		exclude: [ 'node_modules' ],
+	},
+})
+const hotMiddleware = webpackHotMiddleware(compiler)
+
 const frontMiddleware = app => {
-	const compiler = webpack(config)
-	app.use(require('webpack-dev-middleware')(compiler, {
-		publicPath: config.output.publicPath,
-		stats: {
-			cached: false,
-			cachedAssets: false,
-			colors: true,
-			exclude: ['node_modules'],
-		},
-	}))
-	app.use(require('webpack-hot-middleware')(compiler))
+	app.use(devMiddleware)
+	app.use(hotMiddleware)
 }
 
 export default frontMiddleware
